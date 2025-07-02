1e9bc8eb95f4f22593900ba598e2f7985c605f79
Marcotd24 <marco2001pambi@gmail.com>
2025-07-01 22:13:32 -0500

    Semana06
    Semana06

---
+++ Semana06/ejercicios-propuestos-de-listas-enlazada/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+class Nodo
+{
+    public int Dato { get; set; }
+    public Nodo Siguiente { get; set; }
+
+    public Nodo(int dato)
+    {
+        this.Dato = dato;
+        this.Siguiente = null;
+    }
+}
+
+class ListaEnlazada
+{
+    public Nodo cabeza;
+
+    public ListaEnlazada()
+    {
+        cabeza = null;
+    }
+
+    public void Agregar(int dato)
+    {
+        Nodo nuevo = new Nodo(dato);
+        if (cabeza == null)
+        {
+            cabeza = nuevo;
+        }
+        else
+        {
+            Nodo actual = cabeza;
+            while (actual.Siguiente != null)
+            {
+                actual = actual.Siguiente;
+            }
+            actual.Siguiente = nuevo;
+        }
+    }
+
+    public void Mostrar()
+    {
+        Nodo actual = cabeza;
+        while (actual != null)
+        {
+            Console.Write(actual.Dato + " -> ");
+            actual = actual.Siguiente;
+        }
+        Console.WriteLine("null");
+    }
+
+    public int ContarElementos()
+    {
+        int contador = 0;
+        Nodo actual = cabeza;
+        while (actual != null)
+        {
+            contador++;
+            actual = actual.Siguiente;
+        }
+        return contador;
+    }
+
+    public void Invertir()
+    {
+        Nodo anterior = null;
+        Nodo actual = cabeza;
+        Nodo siguiente = null;
+
+        while (actual != null)
+        {
+            siguiente = actual.Siguiente;
+            actual.Siguiente = anterior;
+            anterior = actual;
+            actual = siguiente;
+        }
+        cabeza = anterior;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ListaEnlazada lista = new ListaEnlazada();
+
+        Console.WriteLine("Agregando elementos a la lista...");
+        lista.Agregar(10);
+        lista.Agregar(20);
+        lista.Agregar(30);
+        lista.Agregar(40);
+        lista.Agregar(50);
+
+        Console.WriteLine("\nLista original:");
+        lista.Mostrar();
+
+        Console.WriteLine($"\nNúmero de elementos en la lista: {lista.ContarElementos()}");
+
+        Console.WriteLine("\nInvirtiendo la lista...");
+        lista.Invertir();
+
+        Console.WriteLine("\nLista invertida:");
+        lista.Mostrar();
+    }
+}
